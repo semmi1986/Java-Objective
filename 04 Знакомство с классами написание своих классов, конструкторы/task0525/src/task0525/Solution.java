6977cf38f5714cdc2cee64092f7c3fd602159e67
Samosiuk Siamion <samasiuk1986@gmail.com>
2024-07-26 13:52:55 +0300

    feat: add solution task 0525

--- 04 Знакомство с классами написание своих классов, конструкторы/task0525/src/task0525/Solution.java
+++ 04 Знакомство с классами написание своих классов, конструкторы/task0525/src/task0525/Solution.java
@@ -24,6 +24,16 @@ public class Solution {
         System.out.println(duck1);
         System.out.println(duck2);
 
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog();
+        System.out.println(dog1);
+        System.out.println(dog2);
+
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+        System.out.println(cat1);
+        System.out.println(cat2);
+
         //напишите тут ваш код
     }
 
@@ -34,4 +44,16 @@ public class Solution {
     }
 
     //напишите тут ваш код
+
+    public static class Dog {
+        public String toString() {
+            return "Dog";
+        }
+    }
+
+    public static class Cat {
+        public String toString() {
+            return "Cat";
+        }
+    }
 }
